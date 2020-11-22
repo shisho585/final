a03b38457fc4d2f6f8b3892065d552df3b4443df
shisho home <shisho585@gmail.com>
2020-11-22 20:51:35 +0200

    return back

--- server-nest/src/db/db.module.ts
+++ server-nest/src/db/db.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Plain } from './entities/plain.entity';
+import { Flight } from './entities/flight.entity';
+import { User } from './entities/user.entity';
+import { Ticket } from './entities/ticket.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'test_flys',
+      entities: [Plain, Flight, User, Ticket],
+      synchronize: true,
+      logging: true,
+    }),
   ],
 })
-export class DbModule { }
+export class DbModule {}
