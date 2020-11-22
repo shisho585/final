cecb7495a45f69948e806d7487c2192b09afc366
shisho home <shisho585@gmail.com>
2020-11-22 19:15:18 +0200

    typeorm seperate configuration

--- server-nest/src/db/db.module.ts
+++ server-nest/src/db/db.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Plain } from './entities/plain.entity';
-import { Flight } from './entities/flight.entity';
-import { User } from './entities/user.entity';
-import { Ticket } from './entities/ticket.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'test_flys',
-      entities: [Plain, Flight, User, Ticket],
-      synchronize: true,
-      logging: true,
-    }),
+    TypeOrmModule.forRoot(),
   ],
 })
-export class DbModule {}
+export class DbModule { }
